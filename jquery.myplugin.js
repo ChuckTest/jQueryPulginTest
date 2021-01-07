98cd1f8d127dcc22c4947d39375e767e2722d401
Chuck Lu <chuck.lu@qq.com>
2021-01-07 22:57:54 +0800

    extend options to {} to keep the defaults

--- jquery.myplugin.js
+++ jquery.myplugin.js
@@ -3,14 +3,16 @@ $.fn.myPlugin = function(options) {
         'color': 'red',
         'fontSize': '12px'
     }
-    var settings = $.extend(defaults, options);
+    var settings = $.extend({},defaults, options);
     //在这里面,this指的是用jQuery选中的元素
     that = this;
     var result = that.css({
         'color': settings.color,
         'fontSize': settings.fontSize
     });
-    console.log(defaults.color);
-    console.log(defaults.fontSize);
+    console.log(`defaults.color = ${defaults.color}`);
+    console.log(`defaults.fontSize = ${defaults.fontSize}`);
+    console.log(`settings.color = ${settings.color}`);
+    console.log(`settings.fontSize = ${settings.fontSize}`);
     return result;
 }
