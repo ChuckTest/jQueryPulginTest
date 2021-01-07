09952051ae2f035e76d5368f2db4802e9029064d
Chuck Lu <chuck.lu@qq.com>
2021-01-07 22:29:48 +0800

    iterate the elements

--- jquery.myplugin.js
+++ jquery.myplugin.js
@@ -1,5 +1,12 @@
 $.fn.myPlugin = function() {
     //在这里面,this指的是用jQuery选中的元素
-    //example :$('a'),则this=$('a')
-    this.css('color', 'red');
+    that = this;
+    that.css('color', 'red');
+    that.each(function() {
+        //对每个元素进行操作
+        //'this' means a html element in each funtion
+        //$(this) convert a html element to a jQuery object
+        var $this = $(this)
+        $this.append(' ' + $this.attr('href'));
+    });
 }
